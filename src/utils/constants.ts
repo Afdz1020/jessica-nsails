b66090ca7f8dc9bc8b2294733a519417bb173e0d
Afdz1020 <Hallpipe10@gmail.com>
2024-01-19 19:29:25 -0500

    is added the list of prices

---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const spaServices = {
+  semipermanente: {
+    descriptions: ['Manicura', 'Express', 'Pedicure', 'Dipping'],
+    prices: ['$43.000', '$38.000', '$40.000', '$50.000'],
+  },
+  acrilico: {
+    descriptions: ['Forrado', 'Esculpidas desde...'],
+    prices: ['$80.000', '$100.000'],
+  },
+  retoques: {
+    descriptions: ['Forrado', 'Acrilicas Esculpidas'],
+    prices: ['$65.000', '$75.000'],
+  },
+  tradicional: {
+    descriptions: ['Manicura', 'Pedicura'],
+    prices: ['$20.000', '$25.000'],
+  },
+};
